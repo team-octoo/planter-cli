0f7fd3edcc56a4ecce89ed30f4e3cce8b43fd2d0
Jos De Berdt <jos.deberdt@sweetmustard.be>
2022-07-27 11:17:09 +0200

    Added RN fonts config & test

---
+++ bin/reactnative/fonts.mjs
@@ -0,0 +1,18 @@
+import chalk from "chalk";
+import { files } from "../helpers/files.mjs";
+import path from "path";
+import { DIRNAME } from "../helpers/globals/globals.js";
+
+export const fonts = {
+  install: () => {
+    return new Promise((resolve, reject) => {
+      console.log(chalk.green("Copying fonts config file..."));
+
+      files.copyFolder(
+        path.join(DIRNAME, "..", "..", "reactnative", "examples", "config", "react-native.config.js"),
+        path.join(process.cwd(), "react-native.config.js")
+      );
+      resolve("Fonts config file copied....");
+    });
+  },
+};
