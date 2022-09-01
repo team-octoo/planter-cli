9040df823cded7c90443c625518bbe0974bb586e
Lennert De Ryck <hello@lennertderyck.be>
2022-09-01 13:08:09 +0200

    Moved docs to new repo

--- docs/src/components/elements/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as SideMenu } from './SideMenu/SideMenu';
-export { default as ExspansionPane } from './ExspansionPane/ExspansionPane';
-export { default as Header } from './Header/Header';
