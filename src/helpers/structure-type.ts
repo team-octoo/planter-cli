ab7ce1f82c88f19e079df809a66c579f5657280c
Jos De Berdt <jozzeh@gmail.com>
2024-02-21 10:34:58 +0100

    fix double test file names

--- src/helpers/structure-type.ts
+++ src/helpers/structure-type.ts
@@ -10,17 +10,17 @@ export function getComponentStructureConfig(structure: StructureType): PlanterCo
       basics: {
         component: "src/components/basics/@pascalCase/@pascalCase.@ext",
         style: "src/components/basics/@pascalCase/@pascalCase.@ext",
-        test: "src/components/basics/@pascalCase/tests/@pascalCase.test.@ext",
+        test: "src/components/basics/@pascalCase/tests/@pascalCase.@ext",
       },
       elements: {
         component: "src/components/elements/@pascalCase/@pascalCase.@ext",
         style: "src/components/elements/@pascalCase/@pascalCase.@ext",
-        test: "src/components/elements/@pascalCase/tests/@pascalCase.test.@ext",
+        test: "src/components/elements/@pascalCase/tests/@pascalCase.@ext",
       },
       pages: {
         component: "src/components/pages/@pascalCase/@pascalCase.@ext",
         style: "src/components/pages/@pascalCase/@pascalCase.@ext",
-        test: "src/components/pages/@pascalCase/tests/@pascalCase.test.@ext",
+        test: "src/components/pages/@pascalCase/tests/@pascalCase.@ext",
       },
     };
   if (structure === "Atomic")
@@ -28,27 +28,27 @@ export function getComponentStructureConfig(structure: StructureType): PlanterCo
       atoms: {
         component: "src/components/atoms/@pascalCase/@pascalCase.@ext",
         style: "src/components/atoms/@pascalCase/@pascalCase.@ext",
-        test: "src/components/atoms/@pascalCase/tests/@pascalCase.test.@ext",
+        test: "src/components/atoms/@pascalCase/tests/@pascalCase.@ext",
       },
       molecules: {
         component: "src/components/molecules/@pascalCase/@pascalCase.@ext",
         style: "src/components/molecules/@pascalCase/@pascalCase.@ext",
-        test: "src/components/molecules/@pascalCase/tests/@pascalCase.test.@ext",
+        test: "src/components/molecules/@pascalCase/tests/@pascalCase.@ext",
       },
       organisms: {
         component: "src/components/organisms/@pascalCase/@pascalCase.@ext",
         style: "src/components/organisms/@pascalCase/@pascalCase.@ext",
-        test: "src/components/organisms/@pascalCase/tests/@pascalCase.test.@ext",
+        test: "src/components/organisms/@pascalCase/tests/@pascalCase.@ext",
       },
       templates: {
         component: "src/components/templates/@pascalCase/@pascalCase.@ext",
         style: "src/components/templates/@pascalCase/@pascalCase.@ext",
-        test: "src/components/templates/@pascalCase/tests/@pascalCase.test.@ext",
+        test: "src/components/templates/@pascalCase/tests/@pascalCase.@ext",
       },
       pages: {
         component: "src/components/pages/@pascalCase/@pascalCase.@ext",
         style: "src/components/pages/@pascalCase/@pascalCase.@ext",
-        test: "src/components/pages/@pascalCase/tests/@pascalCase.test.@ext",
+        test: "src/components/pages/@pascalCase/tests/@pascalCase.@ext",
       },
     };
   if (structure === "Custom") {
